0a4bd75ca2a0273a96a375e4ab8f74fb6099a76f
Shawn Hempel <shawn+github@hempel.cx>
2024-07-22 11:40:21 -0700

    Update EncodingManager.cs to support gpt-4o-mini

--- TiktokenSharp/Services/EncodingManager.cs
+++ TiktokenSharp/Services/EncodingManager.cs
@@ -36,6 +36,7 @@ namespace TiktokenSharp.Services
                                                             // chat
                                                             { "gpt-4", "cl100k_base" },
                                                             { "gpt-4o", "o200k_base" },
+                                                            { "gpt-4o-mini", "o200k_base" },
                                                             { "gpt-3.5-turbo", "cl100k_base" },
                                                             { "gpt-3.5-turbo-16k", "cl100k_base" },
 
